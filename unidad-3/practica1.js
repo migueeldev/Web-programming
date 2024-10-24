fb758b4deb5d325a8ccf4cabbb58c6c5526eefac
migueeldev <migueeldev@gmail.com>
2024-10-24 14:03:39 -0600

    Agrego Proyecto de Unidad y introduccion a Javascript

---
+++ unidad-3/practica1.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    document.getElementById('demo3').style.fontSize='35px';
+    document.getElementById('demo3').innerHTML = "Cambio de 35 estremo a color ROJO y a 35px";
+    document.getElementById('demo3').style.color = "red";
+}
+
+function aumentar() {
+    document.getElementById('demo2').style.fontSize="35px"
+    document.getElementById('demo2').style.color="red"
+}
---
+++ unidad-3/practica2Producto.js
@@ -0,0 +1,29 @@
+function showFront() {
+    document.getElementById("productImage").src = "img/frente.webp";
+    document.getElementById("productImage").alt = "Producto Frente";
+}
+
+function showLeft() {
+    document.getElementById("productImage").src = "img/izquierda.webp";
+    document.getElementById("productImage").alt = "Producto Izquierda";
+}
+
+function showRight() {
+    document.getElementById("productImage").src = "img/derecha.webp";
+    document.getElementById("productImage").alt = "Producto Derecha";
+}
+
+function showBack() {
+    document.getElementById("productImage").src = "img/atras.webp";
+    document.getElementById("productImage").alt = "Producto Detrás";
+}
+
+function showInfo() {
+    document.getElementById("productInfo").classList.remove("d-none");
+    document.getElementById("productPrice").classList.add("d-none");
+}
+
+function showPrice() {
+    document.getElementById("productPrice").classList.remove("d-none");
+    document.getElementById("productInfo").classList.add("d-none");
+}
